5dab378ec4eb36173641d9c8dc5d52449ee9f8d7
Lara Corkrey <laracorkrey@gmail.com>
2018-04-08 18:35:31 +0000

    added simple form

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -8,4 +8,15 @@ class FoodsController < ApplicationController
     @foods = Food.new
   end
 
+  def create
+    Food.create(food_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :description, :country)
+  end
+
 end
